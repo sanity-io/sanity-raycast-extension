ca1b7f9c27db9fdcb01c29918d76c3d15da027cf
Knut Melvær <knut@sanity.io>
2023-03-20 09:27:29 +0100

    Add more details to project members

--- src/actions/projects/members/ListMembers.tsx
+++ src/actions/projects/members/ListMembers.tsx
@@ -1,14 +1,31 @@
 import { useEffect, useState } from "react";
-import { ActionPanel, Action, List, Cache, Image } from "@raycast/api";
+import { ActionPanel, Action, List, Cache, Image, Icon } from "@raycast/api";
 import { SanityProject, SanityUser } from "@sanity/client";
 import { projectClient } from "../../../util/client";
+
+interface SanityExtendedUser extends SanityUser {
+  email: string;
+  provider: "google" | "github" | "sanity" | "sso";
+  isCurrentUser: boolean;
+}
+
+const iconResolver = (provider: string) => {
+  switch (provider) {
+    case "google":
+      return "https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg";
+    case "github":
+      return "https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/github.svg";
+    default:
+      return "https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/mail.svg";
+  }
+};
 
 const cache = new Cache();
 export function ListMembers(props: { project: SanityProject }) {
   const { project } = props;
   const membersList = project.members.map(({ id }: { id: string }) => id).join(",");
   const cacheKey = `${project.id}-membersList`;
-  const [members, setMembers] = useState<SanityUser[] | null>(null);
+  const [members, setMembers] = useState<SanityExtendedUser[] | null>(null);
   useEffect(() => {
     async function fetchMembers() {
       const cachedMembers = cache.get(cacheKey);
@@ -25,7 +42,7 @@ export function ListMembers(props: { project: SanityProject }) {
     fetchMembers();
   }, []);
   return (
-    <List filtering={true} isLoading={!members}>
+    <List filtering={true} isLoading={!members} isShowingDetail>
       {members &&
         members.map((member) => {
           return (
@@ -35,8 +52,28 @@ export function ListMembers(props: { project: SanityProject }) {
               title={member.displayName}
               actions={
                 <ActionPanel>
-                  <Action title="Select User" onAction={() => null} />
+                  {member.email && <Action.CopyToClipboard title="Copy Email" content={member.email} />}
                 </ActionPanel>
+              }
+              detail={
+                <List.Item.Detail
+                  markdown={member.imageUrl && `![${member.displayName}](${member.imageUrl})`}
+                  metadata={
+                    <List.Item.Detail.Metadata>
+                      {member.isCurrentUser && (
+                        <List.Item.Detail.Metadata.Label title="Is it you?" icon={Icon.CheckCircle} />
+                      )}
+                      <List.Item.Detail.Metadata.Label title="Name" text={member.givenName + " " + member.familyName} />
+                      {member.email && (
+                        <List.Item.Detail.Metadata.Label
+                          title="Email"
+                          text={member.email}
+                          icon={iconResolver(member.provider)}
+                        />
+                      )}
+                    </List.Item.Detail.Metadata>
+                  }
+                />
               }
             ></List.Item>
           );
---
+++ src/actions/projects/members/index.ts
@@ -0,0 +1,1 @@
+export { ListMembers } from "./ListMembers";
